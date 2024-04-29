79afa733c870bbe7289ae5f81433ecaa5b5c6c3b
Kaiky Vasques <156720570+kaiky-dev@users.noreply.github.com>
2024-04-29 15:34:54 -0300

    Comentando código
    Código comentado e tentando resolver o bug.

--- Protótipo/js/index.js
+++ Protótipo/js/index.js
@@ -1,46 +1,60 @@
+// Visor
 //Buscando elementos
+const result = document.getElementsByClassName("result")[0]; //Resultado
+const display = document.getElementsByClassName("display"); //Inputs 
+let currentDisplay = display[0] //Input Atual (calculadora)
 
-// visor
-const result = document.getElementsByClassName("result")[0];
-let display = document.getElementsByClassName("display");
-let currentDisplay = display[0]
-let calc;
-
-
-// buttons
+// Buscando botões
 const button = document.getElementsByTagName("button");
 
-let condicion = true;
+// Botões dos calculos de saúde
+let condicion = true; //Condição para o if
+let currentCalc = calculation; //Calculo atual
+
 button[0].addEventListener("click", () => {
 
     if (condicion) {
-        display[0].classList.add("none");
-        currentDisplay.value = "";
-        display[1].classList.remove("none");
-        currentCalc = waterCalc
-        currentDisplay = display[1]
-        console.log(currentDisplay)
-        button[21].addEventListener('click', currentCalc); //Calculando resultado
-        condicion = false;
+        display[0].classList.add("none"); //Remove visualmente o input da calculadora
+        currentDisplay.value = ""; //Limpa o input atual (calculadora)
 
+        display[1].classList.remove("none"); //Adiciona visualmente o input da água
+        currentCalc = waterCalc; //Calculo atual vira o da água
+        currentDisplay = display[1]; //Input atual agora é da água
+        
+        button[21].addEventListener("click", currentCalc); //Botão de calcular (=)
+        
+        condicion = false; //Muda o valor da condição
 
-    }
+    } //Condição para remover o input(display) da calculadora e adicionar o da água
 
     else if (condicion == false) {
-        display[1].classList.add("none");
-        currentDisplay.value = "";
-        display[0].classList.remove("none");
-        currentCalc = calculation;
-        currentDisplay = display[0];
-        console.log(currentDisplay)
-        button[21].addEventListener('click', currentCalc); //Calculando resultado
-        condicion = true;
-    }
-})
+        display[1].classList.add("none"); //Remove visualmente o input da água
+        currentDisplay.value = ""; //Limpa o input atual (água)
+
+        display[0].classList.remove("none"); //Adiciona visualmente o input da calculadora
+        currentCalc = calculation; //Calculo atual vira de calculadora
+        currentDisplay = display[0]; //Input atual agora é o da calculadora
+
+        button[21].addEventListener("click", currentCalc); //Botão de calcular (=)
+
+        condicion = true; //Muda o valor da condição
+
+    } //Condição para remover o input(display) da água e adicionar o da calculadora
+
+    else{
+        console.log("Porra.. Ocorreu um erro.")
+    } //Se ocorrer algum erro nas condições
+
+}) //Botão do calculo da água e sua função
+
 // button[1]
 // button[2]
+
+//Botões de limpar input
 button[3].addEventListener("click", () => currentDisplay.value = ""); //AC
 button[4].addEventListener("click", () => currentDisplay.value = currentDisplay.value.slice(0, -1));//DEL
+
+//Botões de numeros e operações
 button[5].addEventListener("click", () => currentDisplay.value += "%"); //%
 button[6].addEventListener("click", () => currentDisplay.value += "/"); // / 
 button[7].addEventListener("click", () => currentDisplay.value += 7); //7
@@ -57,20 +71,23 @@ button[17].addEventListener("click", () => currentDisplay.value += 3); //3
 button[18].addEventListener("click", () => currentDisplay.value += "+"); //+
 button[19].addEventListener("click", () => currentDisplay.value += "."); //.
 button[20].addEventListener("click", () => currentDisplay.value += 0); //0
-button[21].addEventListener('click', calculation); //Calculando resultado
+
+button[21].addEventListener("click", currentCalc); //=
 
 
 //Funções
-let currentCalc;
+let calc; //Calculo
+
 function calculation() {
-    calc = eval(currentDisplay.value);
+    calc = eval(currentDisplay.value); //Calculo
     console.log(calc);
-    result.textContent = calc
-}
+    result.textContent = calc //Mostrando resultado
+
+}; //Calculadora normal
 
 function waterCalc() {
-    calc = currentDisplay.value * 30 + ("ml");
+    calc = currentDisplay.value * 30 + ("ml"); //Calculo
     console.log(calc);
-    result.textContent = calc
-}
+    result.textContent = calc //Mostrando resultado
+} //Calculadora de água
 
